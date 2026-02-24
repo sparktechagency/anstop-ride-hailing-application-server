ce4dc2c277bf9d5b75fa73c8f173970f9e8cb875
abubakar <abubakar850772@gmail.com>
2026-02-24 12:15:49 +0600

    feat(transaction): update transaction model and validation; add rideId and transactionId fields, enhance payout details

--- src/modules/transaction/transaction.model.ts
+++ src/modules/transaction/transaction.model.ts
@@ -5,61 +5,42 @@ import { TRANSACTION_STATUS, TRANSACTION_TYPE } from "./transaction.constant";
 
 const transactionSchema = new Schema<TTransaction, ITransactionModel>(
   {
-
-    riderId: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+    transactionId: {
+      type: String,
       required: true,
-      index: true,
+      unique: true,
     },
-
-    driverId: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      index: true,
-    },
-
     amount: {
       type: Number,
       required: true,
-      min: 0,
     },
-
-    commissionRate: {
-      type: Number, // %
-      min: 0,
-      max: 100,
-    },
-
-    commissionAmount: {
-      type: Number, // actual system earning
-      min: 0,
-    },
-
-    driverEarningAmount: {
-      type: Number, // withdrawable earning
-      min: 0,
-    },
-
     type: {
       type: String,
       enum: Object.values(TRANSACTION_TYPE),
       required: true,
-      index: true,
     },
-
     status: {
       type: String,
       enum: Object.values(TRANSACTION_STATUS),
       required: true,
-      index: true,
     },
-
     payoutDetails: {
-      accountNumber: String,
-      accountHolderName: String,
-      accountType: String,
-      bankName: String,
+      accountNumber: {
+        type: String,
+      },
+      accountHolderName: {
+        type: String,
+      },
+      accountType: {
+        type: String,
+      },
+      bankName: {
+        type: String,
+      },
+    },
+    rideId: {
+      type: Schema.Types.ObjectId,
+      ref: "Ride",
     },
   },
   {
