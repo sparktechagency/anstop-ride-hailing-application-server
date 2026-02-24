c510ca09ca74a062d47af2fe2b5c75c46ce23091
abubakar <abubakar850772@gmail.com>
2026-02-24 12:19:11 +0600

    refactor(sendSMS): remove commented-out OTP sending and verification functions

--- src/utils/sendSMS.ts
+++ src/utils/sendSMS.ts
@@ -9,40 +9,6 @@ const twilioClient = new Twilio(apiKey, secret, {
   accountSid: accountSid
 });
 
-// export async function sendOtpToUser(phoneNumber: string): Promise<void> {
-//   console.log("Twilio Verification Service SID:", config.twilio_verification_service_sid);
-//   try {
-//     const response = await twilioClient.verify.v2.services(config.twilio_verification_service_sid!).verifications.create({
-//       to: phoneNumber,
-//       channel: 'sms',
-//     });
-  
-//     console.log(response);
-// } catch (error) {
-//     console.error("Error sending OTP:", error);
-//     throw new ApiError(500,"Failed to send OTP");
-//   }
-  
-// }
-
-// // controllers/auth.ts
-// export const verifyOtp = async (phone: string, code: string) => {
-//   try {
-//     const verification_check = await twilioClient.verify.v2
-//       .services(process.env.TWILIO_VERIFY_SERVICE_SID!)
-//       .verificationChecks.create({
-//         to: phone,
-//         code,
-//       });
-
-//     if (verification_check.status !== "approved") {
-//       throw new ApiError(400, "Invalid code");
-//     }
-//   } catch (error) {
-//     console.error("OTP Verify Error:", error);
-//     throw new ApiError(500, "OTP verification failed");
-//   }
-// };
 
 
 export async function sendOtpToUser(phoneNumber: string, otp: string): Promise<void> {
